6983a4894fac2280a06b440100620a28cd7bee2a
M-Pomroy <molly.e.pomroy@gmail.com>
2025-04-09 20:31:48 +0100

    Add header and footer and update meta tags

---
+++ src/components/Footer.js
@@ -0,0 +1,52 @@
+import logo from "../assets/logo-footer.svg";
+import "../styles/footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container footer-wrap">
+        <img src={logo} alt="Little Lemon logo" />
+        <div className="footer-menu-wrap">
+          <nav className="footer-menu">
+            <h3>Doormat Navigation</h3>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/menu">Menu</a>
+              </li>
+              <li>
+                <a href="/reservations">Reservations</a>
+              </li>
+              <li>
+                <a href="/order-online">Order Online</a>
+              </li>
+              <li>
+                <a href="/login">Login</a>
+              </li>
+            </ul>
+          </nav>
+          <nav className="footer-menu">
+            <h3>Contact</h3>
+            <ul>
+              <li>
+                <a href="tel:01265447852">+44 (0) 1265 447 852</a>
+              </li>
+              <li>
+                <a href="mailto:contact@littlelemon.com">
+                  contact@littlelemon.com
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import logo from "../assets/logo.svg";
+import Nav from "./Nav";
+import "../styles/header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="container header-wrap">
+        <img src={logo} alt="Little Lemon logo" />
+        <Nav />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
