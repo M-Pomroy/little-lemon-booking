ebf3ed7a9aa9dbe33f73995301e8d531e86c7c5b
M-Pomroy <molly.e.pomroy@gmail.com>
2025-04-11 16:41:35 +0100

    Add the about section to the homepage

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,39 @@
-const About = () => {};
+import "../styles/about.css";
+import mario_adrian from "../assets/mario-adrian.jpg";
+import fish from "../assets/fish.jpg";
+
+const About = () => {
+  return (
+    <section className="section">
+      <div className="container container--860 about-wrap">
+        <div class="about-text">
+          <h2>Little Lemon</h2>
+          <p className="subtitle subtitle--small">Chicago</p>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </p>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit.
+          </p>
+        </div>
+        <div className="about-img-wrap">
+          <img
+            src={mario_adrian}
+            alt="Mario and Adrian - the owners of Little Lemon"
+            className="about-img-right"
+          />
+          <img
+            src={fish}
+            alt="Fish cooking on a grill with tomatoes, potatoes, and a slice of lemon"
+            className="about-img-left"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default About;
