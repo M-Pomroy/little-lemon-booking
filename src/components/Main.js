89bf2a381951ada7621dbb43933edcf55fcfae94
M-Pomroy <molly.e.pomroy@gmail.com>
2025-04-12 14:31:33 +0100

    Move the api functions to their own file

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,43 +1,16 @@
-import "../styles/main.css";
+import { Routes, Route } from "react-router-dom";
+import { useReducer } from "react";
 import Booking from "./Booking";
 import Homepage from "./Homepage";
 import AboutPage from "./AboutPage";
 import MenuPage from "./MenuPage";
 import BookingConfirmation from "./BookingConfirmation";
-import { Routes, Route } from "react-router-dom";
-import { useReducer } from "react";
-
-const submitAPI = function (formData) {
-  return true;
-};
+import { fetchAPI, submitAPI } from "../api/formApi";
+import "../styles/main.css";
 
 const timesReducer = (state, action) => {
   let date = new Date(action);
   return fetchAPI(date);
-};
-
-const seededRandom = function (seed) {
-  var m = 2 ** 35 - 31;
-  var a = 185852;
-  var s = seed % m;
-  return function () {
-    return (s = (s * a) % m) / m;
-  };
-};
-
-const fetchAPI = function (date) {
-  let result = [];
-  let random = seededRandom(date.getDate());
-
-  for (let i = 17; i <= 23; i++) {
-    if (random() < 0.5) {
-      result.push(i + ":00");
-    }
-    if (random() < 0.5) {
-      result.push(i + ":30");
-    }
-  }
-  return result;
 };
 
 const today = new Date();
