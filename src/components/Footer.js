6b7e20131cd7b5434e0c183f73d8f4ad008ca545
M-Pomroy <molly.e.pomroy@gmail.com>
2025-05-28 12:02:47 +0100

    Update the footer navigation to use Link elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import logo from "../assets/logo-footer.svg";
 import "../styles/footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -9,26 +10,20 @@ const Footer = () => {
         <div className="footer-menu-wrap">
           <nav className="footer-menu">
             <h2>Doormat Navigation</h2>
-            <ul>
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li>
-                <a href="/about">About</a>
-              </li>
-              <li>
-                <a href="/menu">Menu</a>
-              </li>
-              <li>
-                <a href="/reservations">Reservations</a>
-              </li>
-              <li>
-                <a href="/order-online">Order Online</a>
-              </li>
-              <li>
-                <a href="/login">Login</a>
-              </li>
-            </ul>
+            <div className="footer-menu-list">
+              <Link to="/" className="footer-link">
+                Home
+              </Link>
+              <Link to="/about" className="footer-link">
+                About
+              </Link>
+              <Link to="/menu" className="footer-link">
+                Menu
+              </Link>
+              <Link to="/booking" className="footer-link">
+                Reservations
+              </Link>
+            </div>
           </nav>
           <nav className="footer-menu">
             <h2>Contact</h2>
