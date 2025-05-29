cc5156c9bbda070d72a2ae24004ed4a068ef97ac
M-Pomroy <molly.e.pomroy@gmail.com>
2025-05-29 21:05:47 +0100

    Update the specials section to contain three menu items

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,5 +1,35 @@
 import "../styles/specials.css";
 import Card from "./Card";
+import greek_salad from "../assets/greek-salad.jpg";
+import bruschetta from "../assets/bruschetta.jpg";
+import lemon_dessert from "../assets/lemon-dessert.jpg";
+
+const specials = [
+  {
+    id: 0,
+    img: greek_salad,
+    title: "Greek Salad",
+    price: "12.99",
+    description:
+      "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons",
+  },
+  {
+    id: 1,
+    img: bruschetta,
+    title: "Bruschetta",
+    price: "5.99",
+    description:
+      "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil",
+  },
+  {
+    id: 2,
+    img: lemon_dessert,
+    title: "Lemon Dessert",
+    price: "5.00",
+    description:
+      "This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined",
+  },
+];
 
 const Specials = () => {
   return (
@@ -12,9 +42,9 @@ const Specials = () => {
           </button>
         </div>
         <div className="specials-wrap">
-          <Card />
-          <Card />
-          <Card />
+          {specials?.map((special) => {
+            return <Card key={special.id} {...special} />;
+          })}
         </div>
       </div>
     </section>
